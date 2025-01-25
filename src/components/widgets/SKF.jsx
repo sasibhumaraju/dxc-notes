31c03838eeefd4a02836ec6b1b828ee6fa83ccbe
Bhumaraju <s.bhumaraju@dxc.com>
2025-01-25 22:43:56 +0530

    UI improved

--- src/components/widgets/SKF.jsx
+++ src/components/widgets/SKF.jsx
@@ -1,9 +1,19 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import styles from '../css/Accounts.module.css'
 import AccountListTile from './AccountListTile'
-import { SKFData } from '../../Data'
+import { SKFData, SKFDataSaturday } from '../../Data'
+import GetDayName from '../../util/GetDayName';
 
 function SKF() {
+
+  const [dbCheckList, setDbChecklist] = useState([]);
+
+  useEffect(()=>{
+    var num = new Date().getDay();
+    if(num === 6) setDbChecklist(SKFDataSaturday);
+    else if(num === 0) setDbChecklist([])
+    else setDbChecklist(SKFData)
+  },[])
 
   useEffect(()=>{
     new Date().toLocaleDateString('en-US',{
@@ -17,10 +27,11 @@ function SKF() {
 
   return (
     <div className={styles.account_container}>  
-        {SKFData.map((obj,_)=>{
+        {dbCheckList.map((obj,_)=>{
             return <AccountListTile key={_} no={obj.no} beforeTime={obj.beforeTime} cetTime={obj.cetTime} afterTime={obj.afterTime} region={obj.region} imsRegion={obj.imsRegion}  code={obj.code} keyCode={obj.key}></AccountListTile>
         })}
        
+       { dbCheckList.length === 0 && <div className={styles.sunday_chill}>Chill dude, No db checks during Sunday! 😉 </div> }
     </div>
   )
 }
